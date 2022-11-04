4e048a6ad12832c0059223f36c0cc4d83798d571
willi <willi@LAPTOP-8H8QL02R>
2022-11-03 20:38:50 -0500

    Agregar archivos de proyecto.

---
+++ VentasBarrios2_Prueba/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VentasBarrios2_Prueba")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("VentasBarrios2_Prueba")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("be5a47c4-cb4c-440f-9131-87eb06ddf5db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VentasBarrios2_Prueba/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using ex03_VentasBarrio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VentasBarrios2_Prueba
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IdentificaPromedioBarrio()
+        {
+            //arrange
+            float[] promedioBarrios = new float[] { 55.35f, 58.94f, 58.22f, 54.08f, 48f };
+            string[] nombresBarrios = new string[] { "Robledo", "Laureles", "Robledo", "Pradocentro", "´Poblado" };
+
+            //act
+            float mejorPromedio;
+            string nombreMejorBarrio;
+
+            Program.IdentificaPromedioBarrio(promedioBarrios, nombresBarrios, out mejorPromedio, out nombreMejorBarrio);
+            float cantidadEsperada = 48f;
+            string nombreEsperado = "Pradocentro";
+
+            //assert
+            Assert.AreEqual(cantidadEsperada, mejorPromedio);
+
+            Assert.AreEqual(nombreEsperado, nombreMejorBarrio);
+
+
+        }
+
+        [TestMethod]
+        public void TotalizaPedidosBarrios()
+        {
+            //arrange
+            int cantidadGalletas = { 1120, 1001, 1200, 1560, 1000 };
+            string[] nombresBarrios = new string[] { "Robledo", "Laureles", "Robledo", "Pradocentro", "´Poblado" };
+
+            //act
+            float cantidadPedidos;
+            string nombrePedidosBarrio;
+
+            Program.TotalizaPedidosBarrios(cantidadGalletas, nombresBarrios, out cantidadPedidos, out nombrePedidosBarrio);
+
+            float cantidadEsperada = 1120;
+            string nombreEsperado = "Pradocentro";
+
+            //assert
+            Assert.AreEqual(cantidadEsperada, cantidadPedidos);
+
+            Assert.AreEqual(nombreEsperado, nombrePedidosBarrio);
+
+        }
+    }
+}
